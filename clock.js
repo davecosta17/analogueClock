8901051336dabb7a356fe1b959231d9be4c8500d
David DaCosta <dvddacosta47@gmail.com>
2022-12-17 07:16:57 +0000

    new clock

---
+++ clock.js
@@ -0,0 +1,16 @@
+
+    // Get the clockface element
+    var clockface = document.getElementById('clockface');
+
+    // Get the clock hands
+    var secondHand = clockface.querySelector('.second-hand');
+    var minuteHand = clockface.querySelector('.minute-hand');
+    var hourHand = clockface.querySelector('.hour-hand');
+
+    // Function to update the clock hands
+    function updateClock() {
+      // Get the current time
+      var now = new Date();
+
+      //
+    }
